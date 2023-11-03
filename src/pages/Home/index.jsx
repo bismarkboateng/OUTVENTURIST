46e5fea755c46041bd6b34cca8ad0bcce0d1ab80
Amoh-Ntim <amohntim123@gmail.com>
2023-11-03 21:53:33 +0000

    Home default without testing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,9 @@
-// Home.js
 
-import Footer from '../../components/Footer';
 
-const Home = () => {
+export default function index() {
   return (
-    <div>
-      {/* <h1>Home Page</h1> */}
-      <Footer />
-    </div>
-  );
-};
-
-export default Home;
+    <section>
+        Home component
+    </section>
+  )
+}
